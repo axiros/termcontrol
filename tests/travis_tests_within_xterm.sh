767a39641ec864e0dbe7c91dc4bc89314170f87e
Gunther Klessinger <gunther.klessinger@axiros.com>
2021-05-04 01:24:21 +0200

    ci: travis output

--- tests/travis_tests_within_xterm.sh
+++ tests/travis_tests_within_xterm.sh
@@ -3,5 +3,21 @@
 rm -f success
 d="$(pwd)"
 
-pytest -xs . && touch "$d/success"
+
+function main { 
+
+    test "$1" == "start" && {
+        echo $$ > testpid
+        xterm -l -e "$0" &
+        sleep 1
+        tail -f Xterm.log*
+        exit 0
+    }
+    
+    pytest -xs . && touch "$d/success"
+    kill $(cat "$d/testpid") 
+
+}
+
+main "$@"
 
